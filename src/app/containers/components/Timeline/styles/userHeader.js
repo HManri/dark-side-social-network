d81090f450767891c0083b987b58ed567c2b3403
HManri <hector.manrique.prades@gmail.com>
2023-04-13 09:30:41 +0200

    add back button to not main userHeader

--- src/app/containers/components/Timeline/styles/userHeader.js
+++ src/app/containers/components/Timeline/styles/userHeader.js
@@ -8,16 +8,21 @@ export default (theme) => ({
         borderBottom: `1px solid ${theme.colors.border}`,
         backgroundColor: theme.colors.lightGrey,
     },
+    back: {
+        cursor: 'pointer',
+    },
     photo: {
         width: '128px',
         height: '128px',
         backgroundColor: theme.colors.white,
         border: `1px solid ${theme.colors.border}`,
         borderRadius: '50%',
+        marginLeft: '8px',
         marginRight: '8px',
         textAlign: 'center',
         boxShadow: `2px 2px 5px 0px rgba(0,0,0,0.75)`,
         flexShrink: '0',
+        userSelect: 'none',
 
         '& > img': {
             display: 'inline-block',
